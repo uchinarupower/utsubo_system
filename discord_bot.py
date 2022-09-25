9d9847a75a9c14054db70450f6ac7251108fd63e
uchinarupower <uchikawa.k.th@gmail.com>
2022-09-25 21:35:32 +0900

    token reset

--- discord_bot.py
+++ discord_bot.py
@@ -4,9 +4,8 @@ import utsubo_system
 
 def get_token():
     with open("DISCORD_TOKEN.txt") as f:
-        s = f.read()
-
-    print(s)
+        token = f.read()
+    return token
 
 def connect_discord():
     # 自分のBotのアクセストークンに置き換えてください
@@ -20,48 +19,17 @@ def connect_discord():
     async def on_ready():
         # 起動したらターミナルにログイン通知が表示される
         print(f'login to {client.user} !')
-        channel = client.get_channel(730244126481580075)
-        await channel.send('よろ^^')
-
-    @client.event
-    async def on_message(message):
-        if message.author.bot:
-            return
-
-
-        if message.content.startswith('/team'):
-            await message.channel.send('uoooooooo')
-
-
-    # メッセージ受信時に動作する処理
-    #@client.event
-    #async def on_message(message):
-        # メッセージ送信者がBotだった場合は無視する
-        #if message.author.bot:
-        #    return
-        # 「/neko」と発言したら「にゃーん」が返る処理
-        #print(message.content)
-        #if message.content == f"/neko":
-
-
-    # 返信する非同期関数を定義
-    async def reply(message):
-        reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
-        await message.channel.send(reply) # 返信メッセージを送信
+        #channel = client.get_channel(730244126481580075)
+        #await channel.send('よろ^^')
 
     # 発言時に実行されるイベントハンドラを定義
     @client.event
     async def on_message(message):
-        print(len(message.content))
-
         if message.author.bot:
             return
 
-        if client.user in message.mentions: # 話しかけられたかの判定
-            await reply(message) # 返信する非同期関数を実行
-            await message.channel.send('にゃーん')
+        if message.content.startswith('/us'):
 
-        if message.content.startswith('/last_msg'):
             await message.channel.send('uoooooooo')
 
 
